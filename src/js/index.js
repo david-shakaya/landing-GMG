28baa3309f5179f682f88acd80ac7ddcd03991c8
david-shakaya <devidoz666@gmail.com>
2021-04-10 15:45:02 +0300

    Добавляет стили для слайдера

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
 console.log('hello');
+
+const sliderPercent = document.querySelectorAll('.slider__percent');
+
+// if(sliderPercent.textContent)
+sliderPercent.forEach(el => {
+    if (+el.textContent.slice(0, 1) >= 0) {
+        el.classList.add('positive-balance');
+    } else {
+        el.classList.add('negative-balance');
+    }
+});
+
+// УДаляю точку с кнопки
+const paginationSliderBtn = document.querySelectorAll(
+    '.pagination-slider__btn',
+);
+
+paginationSliderBtn.forEach(btn => (btn.textContent = ''));
